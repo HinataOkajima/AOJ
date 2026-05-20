a6b8e0c7877e21ff5454cd82ea0606b741035060
HinataOkajima <x24022xx@aitech.ac.jp>
2026-05-20 15:35:05 +0900

    a

--- ITP1_2_A.c
+++ ITP1_2_A.c
@@ -4,9 +4,10 @@ int main() {
     int a, b;
 
     scanf("%d%d", &a, &b);
+    
     if(a < b) printf("a < b\n");
     else if (a > b) printf("a > b\n");
     else printf("a == b\n");
-fh
+
     return 0;
 }
