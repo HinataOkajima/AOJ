d9def8c183e2933ebe2ac375d85fb9f376d89d01
HinataOkajima <x24022xx@aitech.ac.jp>
2026-06-02 09:39:57 +0900

    a

---
+++ ITP1_8_A.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char str[1199];
+    fgets(str, 1199, stdin);
+
+    for (int i = 0; str[i] != '\0'; i++){
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            str[i] = str[i] + 32;
+        }else if (str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
